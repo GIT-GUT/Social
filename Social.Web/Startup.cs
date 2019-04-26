059881c60a853a4c06d4969ff3d2984963a822c6
destbg <35627380+destbg@users.noreply.github.com>
2019-04-26 22:50:43 +0300

    Initial

---
+++ Social.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Social.Web.Startup))]
+namespace Social.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
